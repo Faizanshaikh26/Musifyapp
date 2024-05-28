1857b21dd813ba18580ea65724dfd31e887807a9
Faizanshaikh26 <Faizansheikh8209@gmail.com>
2024-05-28 16:17:39 +0530

    ne

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './Home'
-import './App.css'
-import Displayalbums from './albums/Displayalbums'
-import Player from './Components/Player'
+import React, { Suspense, lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import './App.css';
+import Player from './Components/Player';
+import { usePlayer } from './Context/PlayerContext';
+
+const Displayalbums = lazy(() => import('./albums/Displayalbums'));
+
 function App() {
+  const { audioRef } = usePlayer();
+
   return (
     <div className='bg-black text-white h-full'>
-
-<Player/>
-<Routes>
-  <Route path='/'element={<Home/>}/>
-  <Route path='/album/:id' element={<Displayalbums/>}/>
-</Routes>
-
-
+      <Suspense fallback={<div>Ruko Jara Sabar Karo...</div>}>
+        <Player />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/album/:id' element={<Displayalbums />} />
+        </Routes>
+      </Suspense>
+      <audio ref={audioRef} preload='auto' />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
